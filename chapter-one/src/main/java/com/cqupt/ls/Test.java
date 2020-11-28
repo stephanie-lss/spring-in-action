efa46fa0a8e6b793d2d9e3c4e31b6016cca3cfaa
lisheng_glb <goodMorning_glb.com>
2020-11-28 22:52:30 +0800

    first commit

---
+++ chapter-one/src/main/java/com/cqupt/ls/Test.java
@@ -0,0 +1,11 @@
+package com.cqupt.ls;
+
+/**
+ * @author LiSheng
+ * @date 2020/11/28 22:51
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
