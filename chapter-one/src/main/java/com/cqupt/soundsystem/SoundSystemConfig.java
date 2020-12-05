83a26abb58940dbf69f89bdf5a4c4cc6a9bc200f
lisheng <892958173@qq.com>
2020-12-05 14:45:27 +0800

    高级装配

--- chapter-one/src/main/java/com/cqupt/soundsystem/SoundSystemConfig.java
+++ chapter-one/src/main/java/com/cqupt/soundsystem/SoundSystemConfig.java
@@ -1,7 +1,9 @@
 package com.cqupt.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * @author LiSheng
@@ -10,4 +12,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({CDPlayerConfig.class, CDConfig.class})
 public class SoundSystemConfig {
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
---
+++ chapter-one/src/test/java/com/cqupt/soundsystem/Test.java
@@ -0,0 +1,10 @@
+package com.cqupt.soundsystem;
+
+/**
+ * @author LiSheng
+ * @date 2020/12/5 14:09
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
